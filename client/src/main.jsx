3c512cf383fb6d0d5641710cf338b641f817938a
Arnav Tyagi <arnavt65@gmail.com>
2024-11-22 13:44:08 +0530

    Installing shadcnUI components

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Toaster } from "@/components/ui/sonner";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import "./index.css";
+import { SocketProvider } from "./contexts/SocketContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <SocketProvider>
     <App />
-  </StrictMode>,
-)
+    <Toaster closeButton richColors />
+  </SocketProvider>
+);
